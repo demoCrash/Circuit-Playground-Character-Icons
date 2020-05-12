600bf3a8bbc24f619298c5109cbefedb19e66239
demoCrash <173336348@qq.com>
2020-05-12 16:09:39 +0800

    new sprite

--- circuitplayground.ts
+++ circuitplayground.ts
@@ -1,28 +1,6 @@
 namespace circuitplayground {
     //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao1 = image.ofBuffer(hex``);
+    export const greenbg = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao10 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao11 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao12 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao13 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao5 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao6 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao7 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao8 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
-    export const xiongmao9 = image.ofBuffer(hex``);
+    export const sweetgirl = image.ofBuffer(hex``);
 }
